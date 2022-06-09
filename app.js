d3c6cfea58493ea32291bb3305bf67e30c95db6a
andreyrise <andrey.rise@gmail.com>
2022-06-08 22:24:02 -0700

    corect some code

--- app.js
+++ app.js
@@ -26,13 +26,13 @@ async function start() {
     try {
         await mongoose.connect(config.get('mongoUri'), {
             useNewUrlParser: true,
-            useUnifiedTopology: true
+            useUnifiedTopology: true,
+            useCreateIndex: true
         })
         app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`))
     } catch (e) {
         console.log('Server Error', e.message)
         process.exit(1)
     }
-
 }
 start()
